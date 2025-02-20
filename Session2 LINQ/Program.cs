1c321f73859a5b3c8ba6e33a757ba18a38bef88f
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-02-20 17:54:45 +0200

    Set Operators

--- Session2 LINQ/Program.cs
+++ Session2 LINQ/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Runtime.Intrinsics.X86;
 using System.Threading;
 using System.Diagnostics.Metrics;
+using System.Xml;
 
 namespace Session2_LINQ
 {
@@ -193,6 +194,48 @@ namespace Session2_LINQ
             {
                 Console.WriteLine(item);
             }
+            Console.WriteLine("//////");
+            #endregion
+
+            #region LINQ - Set Operators
+            //1. Find the unique Category names from Product List
+            var ress01 = ListGenerator.ProductList.Select(p => p.Category).Distinct();
+            foreach (var item in ress01)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("//////");
+
+            //2.Produce a Sequence containing the unique first letter from both product and customer names.
+            var ress021 = ListGenerator.ProductList.Select(p => p.ProductName[0]).Distinct();
+            var ress022 = ListGenerator.CustomerList.Select(p => p.CustomerName[0]).Distinct();
+            var ress02 = ress021.Union(ress022);
+            foreach(var item in ress02)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("//////");
+
+            //3.Create one sequence that contains the common first letter from both product and customer names.
+            var ress031 = ListGenerator.ProductList.Select(p => p.ProductName[0]).Distinct();
+            var ress032 = ListGenerator.CustomerList.Select(p => p.CustomerName[0]).Distinct();
+            var ress03 = ress031.Intersect(ress032);
+            foreach (var item in ress03)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("//////");
+
+            //4.Create one sequence that contains the first letters of product names that are not also first letters of customer names.
+            var ress041 = ListGenerator.ProductList.Select(p => p.ProductName[0]).Distinct();
+            var ress042 = ListGenerator.CustomerList.Select(p => p.CustomerName[0]).Distinct();
+            var ress04 = ress041.Except(ress042);
+            foreach (var item in ress04)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("//////");
+
             #endregion
         }
     }
